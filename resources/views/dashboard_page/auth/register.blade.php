1db34182039f22fb0d8de048db93cf4504252a8a
h4fizm <farelhafiz.fhm@gmail.com>
2025-08-24 05:46:37 +0700

    modify a little page registration

--- resources/views/dashboard_page/auth/register.blade.php
+++ resources/views/dashboard_page/auth/register.blade.php
@@ -73,8 +73,8 @@
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
               <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('dashboard_template/assets/img/register.jpg'); background-size: cover;">
                 <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+                <h4 class="mt-10 text-white font-weight-bolder position-relative">"Kemudahan Anda Adalah Prioritas Kami."</h4>
+                <p class="text-white position-relative">Silahkan Registrasi Akun Jika Belum Terdaftar.</p>
               </div>
             </div>
           </div>
--- resources/views/dashboard_page/partials/sidebar.blade.php
+++ resources/views/dashboard_page/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0 d-flex align-items-center" href="#">
-      <img src="{{ asset('dashboard_template/assets/img/favicon.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+      <img src="{{ asset('dashboard_template/assets/img/app_logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
       <span class="ms-2 font-weight-bold">Clean & Clear Warehouse</span>
     </a>
   </div>
