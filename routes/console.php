d223daceec19f4df06a5a1b1809e23255348456b
root <denniez.hilmansyah29@gmail.com>
2025-09-30 13:24:50 +0700

    perbaikan reset stock di server

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-// use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 // Schedule::command('stok:reset-bulanan')->everyMinute();
-
+Schedule::command('stock:reset-monthly')->monthlyOn(1, '01:00');
